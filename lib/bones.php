adf00bf6532433cb57ef0aaa2b7cf906f66a881d
Aslof <aslof@gmx.de>
2014-07-11 15:51:20 +0200

    Neu

--- lib/bones.php
+++ lib/bones.php
@@ -3,15 +3,20 @@
 ini_set('display_errors','On');
 error_reporting(E_ERROR | E_PARSE);
 
+define('ROOT', __DIR__ . '/..');
+
 function get($route, $callback) {
 	Bones::register($route, $callback);
 }
 
 class Bones {
 
+	public $content = '';
+	public $vars = array();
 	private static $instance;
 	public static $route_found = false;
 	public $route = '';
+
 	public function __construct() {
 		$this->route = $this->get_route();
 	}
@@ -41,4 +46,21 @@ class Bones {
 			return '/';
 		}
 	}
+
+	public function set($index, $value) {
+		$this->vars[$index] = $value;
+	}
+
+	public function render($view, $layout = "layout") {
+		$this->content = ROOT. '/views/' . $view . '.php';
+		foreach ($this->vars as $key => $value) {
+			$$key = $value;
+		}
+		if (!$layout) {
+			include($this->content);
+		} else {
+			include(ROOT. '/views/' . $layout . '.php');
+		}
+	}
+	
 }
